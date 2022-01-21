48d66e7a4378f47de6d7123c1e994c99974d8a02
MariaKM <merzhanova@gmail.com>
2022-01-21 14:16:35 +0300

    init: reverse digits of this number

--- src/index.js
+++ src/index.js
@@ -1,3 +1,37 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+
+  i = 0;
+  revNumStr = '';
+
+  if (n < 0) {
+    n = n * (-1);
+    n = n.toString();
+
+    while (i < n.length) {
+      revNumStr += n[(n.length - 1) - i];
+
+      i++;
+    }
+
+    // revNumStr = Number(revNumStr) * (-1);
+    // revNumStr = Number(revNumStr);
+
+  } else {
+
+    n = n.toString();
+
+    while (i < n.length) {
+      revNumStr += n[(n.length - 1) - i];
+
+      i++;
+    }
+
+    // revNumStr = Number(revNumStr);
+
+  }
+
+  revNumStr = Number(revNumStr);
+
+  return revNumStr;
+
 }
